9ee0225646503003165caf95f2d9b56cd91c0fe6
Leonardo Grimaldi <leonardo22grimaldi@gmail.com>
2022-05-09 09:44:21 +0200

    added backend for nuovoElettore

--- src/view/nuovoElettore.php
+++ src/view/nuovoElettore.php
@@ -1,3 +1,28 @@
+<?php
+$servername = "localhost";  // Connessione al DB
+$username = "root";
+$password = "";
+$db_name = "db_elezioni";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db_name);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+
+if(isset($_POST['sesso'])) {
+  $sesso = mysqli_real_escape_string($conn, $_POST['sesso']);
+}/*
+else {
+  $_SESSION['errorMessage'] = "Non hai scelto il sesso";
+}*/
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,12 +30,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../style/output.css">
-    <title>Login Scheda</title>
+    <title>Crea nuovo elettore</title>
 </head>
 <body>
+  <?php
+
+if (isset($_SESSION['errorMessage'])) {?>
+  <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+    <strong class="font-bold"><?php echo $_SESSION['errorMessage']?></strong>
+  </div>
+
+  <?php
+    unset($_SESSION['errorMessage']);
+  }?>
 <div class="flex flex-row min-h-screen justify-center items-center">
   <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" action="./nuovoElettore.php" method="POST">
-    <label class="block text-gray-700 text-sm font-bold mb-2" aling="left" for="SEX">
+    <label class="block text-gray-700 text-sm font-bold mb-2" aling="left" for="sesso">
       Sesso:
     </label>
     <div class="flex" aling="left">
@@ -32,6 +67,9 @@
     <div class="mb-4">
       <div class="flex justify-center">
   <div class="mb-3 xl:w-96">
+    <label class="block text-gray-700 text-sm font-bold mb-2" for="seggio">
+        Seggio
+      </label>
     <select class="form-select appearance-none shadow
       block
       w-full
@@ -47,10 +85,22 @@
       ease-in-out
       m-0
       focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example">
-        <option selected>Open this select menu</option>
-        <option value="1">One</option>
-        <option value="2">Two</option>
-        <option value="3">Three</option>
+        <option value="">Seleziona il seggio</option>
+        <?php
+
+        $query = "SELECT * FROM seggio";
+        $result = $conn->query($query);
+        
+        if ($result->num_rows > 0) {
+          while ($row = $result->fetch_assoc()) {
+            ?>
+            <option value="<?php echo $row['CodiceSeggio']?>"><?php echo $row['Indirizzo']." | ".$row['Citta']." | ".$row['CAP']?></option>
+            <?php
+          }
+        }
+        
+        ?>
+        
     </select>
   </div>
 </div>
