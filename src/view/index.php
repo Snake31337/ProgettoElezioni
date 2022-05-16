2d68f8af9553a64949c3fc4f40c61869b63a167e
Leonardo Grimaldi <leonardo22grimaldi@gmail.com>
2022-05-16 22:01:00 +0200

    added background css and index page

--- src/view/index.php
+++ src/view/index.php
@@ -1,3 +1,44 @@
+<?php
+/*
+
+Da fare:
+    - aggiungere partitii senza voti nel grafico
+
+    - grafico per i voti che i singoli candidati hanno ottenuto
+ */
+
+$servername = "localhost";  // Connessione al DB
+$username = "root";
+$password = "";
+$db_name = "db_elezioni";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db_name);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$queryDataElettore = "SELECT COUNT(*) AS NumeroVoti, scheda.CodicePartito, Nome FROM scheda INNER JOIN partito ON scheda.CodicePartito = partito.CodicePartito GROUP BY CodicePartito";
+$result = $conn->query($queryDataElettore);
+
+$nomiPartiti = array();
+$codiciPartiti = array();
+$numeroVotiPartito = array();
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+
+        array_push($nomiPartiti, $row['Nome']);
+        array_push($codiciPartiti, $row['CodicePartito']);
+        array_push($numeroVotiPartito, $row['NumeroVoti']);
+    }
+} else {
+    echo 'Nessun risultato trovato';
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,38 +47,123 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Elezioni</title>
-    <link rel="stylesheet" href="../style/output.css">
+    <link rel="stylesheet" href="../style/index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet">
 </head>
 
 <body>
-    <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded bg-gray-100">
-        <div class="container flex flex-wrap justify-between items-center mx-auto">
-            <a href="./index.php" class="flex items-center">
-                <img src="../images/logo.png" class="mr-3 h-6 sm:h-9" alt="Logo elezioni" />
-                <span class="self-center text-xl font-semibold whitespace-nowrap text-blue-500">Elezioni</span>
-            </a>
-            <button data-collapse-toggle="mobile-menu" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu" aria-expanded="false">
-                <span class="sr-only">Open main menu</span>
-                <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
-                </svg>
-                <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-                </svg>
-            </button>
-            <div class="hidden w-full md:block md:w-auto" id="mobile-menu">
-                <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
-                    <li>
-                        <a href="./index.php" class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
-                    </li>
-                    <li>
-                        <a href="./login.php" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Login</a>
-                    </li>
-                </ul>
-            </div>
+    <div class="navbar">
+        <div class="logo">
+            <h1>Elezioni Europee</h1>
         </div>
-    </nav>
-    <a href="https://www.flaticon.com/free-icons/election" title="election icons">Election icons created by Freepik - Flaticon</a>
+        <div class="elements">
+            <a href="./index.php">Home</a>
+            <a href="./login.php">Login</a>
+        </div>
+    </div>
+
+    <div class="grafici flex">
+        <div class="shadow-lg rounded-lg overflow-hidden w-1/3">
+            <div class="py-3 px-5 bg-gray-50">Numero voti per Partito</div>
+            <canvas class="p-10" id="chartPie"></canvas>
+        </div>
+
+        <!-- Required chart.js -->
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+        <!-- Chart pie -->
+        <script>
+            function getRandomColor(n) {
+                var letters = '0123456789ABCDEF'.split('');
+                var color = '#';
+                var colors = [];
+                for (var j = 0; j < n; j++) {
+                    for (var i = 0; i < 6; i++) {
+                        color += letters[Math.floor(Math.random() * 16)];
+                    }
+                    colors.push(color);
+                    color = '#';
+                }
+                return colors;
+            }
+
+            const dataPie = {
+                labels: <?php echo json_encode($nomiPartiti) ?>, //["JavaScript", "Python", "Ruby"],
+                datasets: [{
+                    label: "Numero voti per partito",
+                    data: <?php echo json_encode($numeroVotiPartito) ?>, //[300, 50, 100],
+                    backgroundColor: getRandomColor(<?php echo count($numeroVotiPartito) ?>),
+                    hoverOffset: 4,
+                }, ],
+            };
+
+            const configPie = {
+                type: "pie",
+                data: dataPie,
+                options: {
+                    plugins: {}
+                },
+            };
+
+            var chartBar = new Chart(document.getElementById("chartPie"), configPie);
+        </script>
+
+
+        <div class="shadow-lg rounded-lg overflow-hidden w-1/2">
+            <div class="py-3 px-5 bg-gray-50">Numero voti per candidato</div>
+            <canvas class="p-10" id="barChart"></canvas>
+        </div>
+
+        <?php
+        $queryDataCandidato = "SELECT COUNT(*) AS NumeroVoti, Nome, Cognome, candidato.CodiceCandidato FROM candidato INNER JOIN vota ON candidato.CodiceCandidato = vota.CodiceCandidato GROUP BY candidato.CodiceCandidato";
+        $result = $conn->query($queryDataCandidato);
+
+        $nomiCandidati = array();
+        $codiciCandidati = array();
+        $numeroVotiCandidato = array();
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+
+                array_push($nomiCandidati, $row['Nome'] . ' ' . $row['Cognome']);
+                array_push($codiciCandidati, $row['CodiceCandidato']);
+                array_push($numeroVotiCandidato, $row['NumeroVoti']);
+            }
+        } else {
+            echo 'Nessun risultato trovato';
+        }
+
+        ?>
+
+        <script>
+            const labels = <?php echo json_encode($nomiCandidati) ?>;
+            const data = {
+                labels: labels,
+                datasets: [{
+                    label: 'Numero voti per candidato',
+                    data: <?php echo json_encode($numeroVotiCandidato) ?>,
+                    backgroundColor: getRandomColor(<?php echo count($numeroVotiCandidato) ?>),
+                    borderWidth: 1,
+                }]
+            };
+
+            const config = {
+                type: 'bar',
+                data: data,
+                options: {
+                    indexAxis: 'y',
+                },
+            };
+
+            var barChart = new Chart(document.getElementById("barChart"), config);
+        </script>
+
+
+
+    </div>
+
 </body>
 
 </html>
